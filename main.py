3b13d97707d725ccd4b0e79f6d0829f4a61eb21e
Ali <Mr.Ali.Fotouhi@gmail.com>
2023-05-13 03:57:07 +0430

    Update main file

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from typing import List, Any
 
 from aiofilm import AIOFilm, Season, Quality
 
-url = input("~ Enter your url: ") if len(args := sys.argv) < 2 else args[1]
-
 
 def get_option(message: str, iterable: List) -> Any:
     print(
@@ -18,26 +16,10 @@ def get_option(message: str, iterable: List) -> Any:
     return iterable[opt - 1]
 
 
+url = input("~ Enter your url: ") if len(args := sys.argv) < 2 else args[1]
 aio = AIOFilm()
 seasons = aio.find_seasons(url)
 season: Season = get_option("Choose your season", seasons)
 quality: Quality = get_option("Choose your quality", season.qualities)
-print(quality.url)
-
-# parse = urlparse(link)
-# base = f"{parse.scheme}://{parse.netloc}"
-#
-# data = requests.get(link).text
-# bs4 = BeautifulSoup(data, "html.parser")
-#
-# for item in bs4.find_all("li"):
-#     try:
-#         name = item["data-name"]
-#         href = urljoin(base, item["data-href"])
-#         if not href.endswith(".mkv"):
-#             continue
-#         print(name, href, sep=" | ")
-#     except KeyError:
-#         pass
-#
-#
+episodes = aio.grab_episodes(quality)
+print(*episodes, sep="\n")
