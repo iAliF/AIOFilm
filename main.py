b3734b0d74866bdfa85c5d93e5075940eac1f22d
Ali <Mr.Ali.Fotouhi@gmail.com>
2023-05-13 03:58:13 +0430

    Update main file

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def get_option(message: str, iterable: List) -> Any:
         *(f"{index + 1}. {item}" for index, item in enumerate(iterable)),
         sep="\n"
     )
-    while not ((opt := input("~ ")).isdigit() and 0 < (opt := int(opt)) <= len(seasons)):
+    while not ((opt := input("~ ")).isdigit() and 0 < (opt := int(opt)) <= len(iterable)):
         print("Invalid option!")
 
     return iterable[opt - 1]
 
 
-url = input("~ Enter your url: ") if len(args := sys.argv) < 2 else args[1]
-aio = AIOFilm()
-seasons = aio.find_seasons(url)
-season: Season = get_option("Choose your season", seasons)
-quality: Quality = get_option("Choose your quality", season.qualities)
-episodes = aio.grab_episodes(quality)
-print(*episodes, sep="\n")
+def main() -> None:
+    url = input("~ Enter your url: ") if len(args := sys.argv) < 2 else args[1]
+    aio = AIOFilm()
+    seasons = aio.find_seasons(url)
+    season: Season = get_option("Choose your season", seasons)
+    quality: Quality = get_option("Choose your quality", season.qualities)
+    episodes = aio.grab_episodes(quality)
+    print(*episodes, sep="\n")
+
+
+if __name__ == '__main__':
+    main()
